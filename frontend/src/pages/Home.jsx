af2d26f9d955c7e78600e3868031bc3d6a037b69
sujalbindra1012 <sujalbindra@gmail.com>
2024-09-29 14:07:51 +0530

    header completed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
     <div>
-      
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
