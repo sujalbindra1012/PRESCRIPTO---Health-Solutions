64e601c6e2f1296cbffb0678946b16297186dfc8
sujalbindra1012 <sujalbindra@gmail.com>
2024-10-08 16:50:21 +0530

    backend started

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import 'dotenv/config';
+
+//app config
+const app=express()
+const port=process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+//middlewares
+
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+
+app.get('/',(req,res)=>{
+    res.send('API WORKING')
+})
+
+app.listen(port,()=>console.log("server started",port))
