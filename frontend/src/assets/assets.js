c6cc1fc2b217a9c9523dae1193a525f4620eaa4d
sujalbindra1012 <sujalbindra@gmail.com>
2025-02-21 14:04:09 +0530

    logo changes

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -15,6 +15,7 @@ import info_icon from './info_icon.svg'
 import upload_icon from './upload_icon.png'
 import stripe_logo from './stripe_logo.png'
 import razorpay_logo from './razorpay_logo.png'
+import logo2 from './logo2.png'
 import doc1 from './doc1.png'
 import doc2 from './doc2.png'
 import doc3 from './doc3.png'
@@ -43,6 +44,7 @@ export const assets = {
     header_img,
     group_profiles,
     logo,
+    logo2,
     chats_icon,
     verified_icon,
     info_icon,
